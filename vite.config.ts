ebdcb077ef18866784ebe79f303e507cf19d4323
Pharouqu <23131066+Pharouqu@users.noreply.github.com>
2025-08-17 20:57:44 +0100

    Setup GitHub Actions deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,15 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ticktock-admin-hub/", // 👈 important for GitHub Pages
+
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
